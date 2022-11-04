2399798b2101eb948367e300ec6f0ed7c622e097
Leon B <leon.borlinghaus@gmail.com>
2022-11-04 22:53:24 +0100

    proxy front without cors, use user agents

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Req, Res, StreamableFile } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { firstValueFrom } from 'rxjs';
+import * as fs from 'fs';
+import { join } from 'path';
 
 const backenUrl = 'fonts.blh.app';
 
@@ -19,20 +21,56 @@ export class AppController {
       const fontUrl = `https://fonts.googleapis.com${
         request.path
       }?${new URLSearchParams(request.query as {}).toString()}`;
-      const { data } = await firstValueFrom(this.httpService.get(fontUrl));
+      const { data } = await firstValueFrom(
+        this.httpService.get(fontUrl, {
+          headers: {
+            'user-agent': request.headers['user-agent'],
+          },
+        }),
+      );
       const css = (data as string).replace(/fonts.gstatic.com/g, backenUrl);
 
-      return response.set({ 'content-type': 'text/css' }).status(200).send(css);
+      return response
+        .set({
+          'content-type': 'text/css',
+        })
+        .status(200)
+        .send(css);
     } else {
       const fontUrl = `https://fonts.gstatic.com${
         request.path
       }?${new URLSearchParams(request.query as {}).toString()}`;
-      const { data } = await firstValueFrom(this.httpService.get(fontUrl));
 
-      return response
-        .set({ 'content-type': `font/${request.path.split('.').pop()}` })
-        .status(200)
-        .send(data);
+      const fontResponse = await firstValueFrom(
+        this.httpService.get(fontUrl, {
+          responseType: 'arraybuffer',
+        }),
+      );
+      const fontPath = join(
+        process.cwd(),
+        `fonts/${request.path.replace(/\//g, '')}`,
+      );
+      const fileExtension = request.path.split('.').pop();
+
+      if (!fs.existsSync(fontPath)) {
+        const writeStream = fs.createWriteStream(fontPath);
+        writeStream.write(fontResponse.data);
+        writeStream.on('finish', () => {
+          console.log('Added new font to cache: ', fontPath);
+          const file = fs.createReadStream(fontPath);
+          response.set({
+            'content-type': `font/${fileExtension}`,
+          });
+          file.pipe(response);
+        });
+        writeStream.end();
+      } else {
+        const file = fs.createReadStream(fontPath);
+        response.set({
+          'content-type': `font/${fileExtension}`,
+        });
+        file.pipe(response);
+      }
     }
   }
 }
